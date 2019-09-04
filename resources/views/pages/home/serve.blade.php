42b1661a81b85473a14c94b4324d4830b228045d
Md Shoaib Ahmed <shoaibmehrab@users.noreply.github.com>
2019-09-04 23:04:29 +0600

    service new design modified

--- resources/views/pages/home/serve.blade.php
+++ resources/views/pages/home/serve.blade.php
@@ -2,32 +2,140 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-6 col-sm-6">
+        <div class="col-md-7 col-sm-5">
             <!-- <h5 class="text-center">Menu</h5> -->
             <!-- Restaurnet Logo -->
-
+            <div class="row">
+                           
+                           <div class="col-md-12">
+                               <th class="align-middle">
+                                   <h6>Menu Items</h6>
+                               </th>
+                           </div>
+                       </div>
             <!-- Menu foreach  -->
             <table class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <div class="row">
-                            <div class="col-md-2">
-                                <td>
-                                    <h6>Serial</h6>
-                                </td>
-                            </div>
-                            <div class="col-md-10">
-                                <th class="align-middle">
-                                    <h6>Menu Items</h6>
-                                </th>
-                            </div>
-                        </div>
-
-                    </tr>
-                </thead>
+              
                 <tbody>
-                    <tr>
-                        <td>1</td>
+                <div>
+                    <div class="row">
+                        <div class="col-md-4"><td>
+                            <!-- <a href="#" data-toggle="modal"><button type="button" value="delete" class="btn btn-success" data-target="#forModal"> </button></a> -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal"
+                                data-target="#exampleModal">
+                                Chicken burger
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">All Chicken Burger</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <!-- Sub-Menu for each  -->
+                                            <table class="table table-striped table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">Name</th>
+
+                                                    </tr>
+
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td scope="row"><button type="button" class="btn btn-primary"
+                                                                style="width:100%"> Cheese Chicken burger (200 tk)
+                                                            </button></td>
+
+
+                                                    </tr>
+                                                    <tr>
+                                                        <td><button type="button" class="btn btn-primary"
+                                                                style="width:100%"> BBQ Chicken burger (250 tk)
+                                                            </button></td>
+                                                    </tr>
+                                                     <tr>
+                                                        <td><button type="button" class="btn btn-primary"
+                                                                style="width:100%"> BBQ Chicken burger (250 tk)
+                                                            </button></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Confirm</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td></div>
+                        <div class="col-md-4"><td>
+                            <!-- <a href="#" data-toggle="modal"><button type="button" value="delete" class="btn btn-success" data-target="#forModal"> </button></a> -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal"
+                                data-target="#exampleModal">
+                                Chicken burger
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">All Chicken Burger</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <!-- Sub-Menu for each  -->
+                                            <table class="table table-striped table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">Name</th>
+
+                                                    </tr>
+
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td scope="row"><button type="button" class="btn btn-primary"
+                                                                style="width:100%"> Cheese Chicken burger (200 tk)
+                                                            </button></td>
+
+
+                                                    </tr>
+                                                    <tr>
+                                                        <td><button type="button" class="btn btn-primary"
+                                                                style="width:100%"> BBQ Chicken burger (250 tk)
+                                                            </button></td>
+                                                    </tr>
+                                                     <tr>
+                                                        <td><button type="button" class="btn btn-primary"
+                                                                style="width:100%"> BBQ Chicken burger (250 tk)
+                                                            </button></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Confirm</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td></div>
+                        <div class="col-md-4">
                         <td>
                             <!-- <a href="#" data-toggle="modal"><button type="button" value="delete" class="btn btn-success" data-target="#forModal"> </button></a> -->
                             <button type="button" class="btn btn-primary" data-toggle="modal"
@@ -69,6 +177,11 @@
                                                                 style="width:100%"> BBQ Chicken burger (250 tk)
                                                             </button></td>
                                                     </tr>
+                                                     <tr>
+                                                        <td><button type="button" class="btn btn-primary"
+                                                                style="width:100%"> BBQ Chicken burger (250 tk)
+                                                            </button></td>
+                                                    </tr>
                                                 </tbody>
                                             </table>
                                         </div>
@@ -81,6 +194,13 @@
                                 </div>
                             </div>
                         </td>
+                        
+                        </div>
+                    </div>
+                </div>
+                    <tr>
+                        
+                        
                     </tr>
 
                 </tbody>
