cb5996358f947d869fcc5b62810f3b105eb65f62
Md Shoaib Ahmed <shoaibmehrab@users.noreply.github.com>
2019-11-03 12:48:12 +0600

    Dashboard ui modification

--- resources/views/pages/home/dashboard.blade.php
+++ resources/views/pages/home/dashboard.blade.php
@@ -1,5 +1,22 @@
 @extends('layout.master')
 @section('content')
+<style>
+.modal-dialog {
+        width: 90%;
+        height: 100%;
+        /*margin: 0;
+        padding: 0;*/
+        max-width: 2150px;
+        
+    }
+
+    .modal-content {
+        height: auto;
+        min-height: 100%;
+        border-radius: 0;
+    }
+
+</style>
     <div class="row">
       <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
         <div class="card card-statistics">
@@ -112,7 +129,7 @@
               </div>
             </div>
             <p class="text-muted mt-3 mb-0">
-              <i class="mdi mdi-reload mr-1" aria-hidden="true"></i> Today's Sell
+            <button class="btn btn-primary" type="button" data-toggle="modal" data-target="#moredetails">More Details <i class="mdi mdi-more mr-1" style="margin-left:5px;" aria-hidden="true"></i></button>
             </p>
           </div>
         </div>
@@ -120,7 +137,7 @@
     </div>
 
 
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-sm-6">
             <div style="text-align: center; margin-bottom: 25px" class="container-fluid table-responsive">
                 <b>Top 5 Sold Item Today</b>
@@ -181,24 +198,29 @@
                 </table>
             </div>
         </div>
-    </div>
+    </div> -->
 
     <div class="row">
         <div class="col-sm-12">
+          <div class="container">
+            <h4 class="text-center">Top 10 selling items</h4>
+          </div>
             <div class="row">
                 <div class="col-sm-12">
                     <div class="pull-right">
-                        <input type="text" name="datetimes" value="">
+                    <label class="label-custom" style="font-size:14px;">Date Range:</label>
+                    <input type="text" class="date-picker date-picker-custom" name="datetimes" value="">
                     </div>
+                    <br>
                 </div>
             </div>
 
-            <div id="foodcount" name="foodcount">
+            <div id="foodcount" name="foodcount" style="margin-top:10px;">
 
             </div>
         </div>
     </div>
-
+<br>
     <div class="row">
       <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 grid-margin stretch-card">
         <div class="card card-statistics">
@@ -313,12 +335,34 @@
               </div>
             </div>
             <p class="text-muted mt-3 mb-0">
-              <i class="mdi mdi-reload mr-1" aria-hidden="true"></i> Current Month's Sell
+             
+              <i class="mdi mdi-reload mr-1" aria-hidden="true"></i> 
             </p>
           </div>
         </div>
       </div>
     </div>
+
+    <!-- Modal -->
+<div class="modal fade" id="moredetails" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Today's sold items</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <!-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div> -->
+    </div>
+  </div>
+</div>
 @endsection
 
 @push('js')
